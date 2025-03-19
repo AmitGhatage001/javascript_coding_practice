8b9c320b79d6d06c7acd9dc73437520760a80408
Amit Ghatage <amitghatage001@gmail.com>
2025-03-19 16:29:05 +0530

    objects and arrays

---
+++ revision/array.js
@@ -0,0 +1,12 @@
+let arr =["Amit Ghatage",true,25.4,undefined,null];
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+let family=["Anil",61,"Kalpana",55,"Anuja",28,"Amit",25];
+
+console.log(family[0]+" "+family[1]);
+console.log(family[2]+" "+family[3]);
+console.log(family[4]+" "+family[5]);
+console.log(family[6]+" "+family[7]);
---
+++ revision/object.js
@@ -0,0 +1,9 @@
+let obj = {
+    name:"Amit",
+    age:25.4
+}
+
+
+console.log(obj.name +" "+ obj.age)
+
+console.log(obj['name'] +" "+obj['age'])
