d4f80b123c5ea8f2c36817286be61967a7d9b713
Amit Ghatage <amitghatage001@gmail.com>
2025-03-19 16:01:50 +0530

    variables

---
+++ revision/variables.js
@@ -0,0 +1,15 @@
+let namei="Amit"
+
+var rich=true;
+
+var age=25.4
+
+let salary=undefined
+
+let loan=null
+
+console.log(namei +" "+ typeof(namei))
+console.log(rich +" "+ typeof(rich))
+console.log(age +" "+ typeof(age))
+console.log(salary +" "+ typeof(salary))
+console.log(loan +" "+ typeof(loan))
