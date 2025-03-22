ca833eb19615f9d56f276cd28b4f6bc81a50f9ed
Amit Ghatage <amitghatage001@gmail.com>
2025-03-22 18:56:14 +0530

    break/continue and iterating over object/array

---
+++ controlflow/break.js
@@ -0,0 +1,9 @@
+let arr = [1,2.3,"kiara",true];
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]=="kiara")
+        break;
+        else{
+            console.log(arr[i]);
+        }
+}
---
+++ controlflow/iteratingover.js
@@ -0,0 +1,12 @@
+
+let obj = {
+    fullname:"Amit",
+    salary:"$300",
+    saas:[
+        "www.neocode360.com","infographics creator-Currently ideation stage-launching in month"
+    ]
+}
+
+for(let key in obj){   
+    console.log(obj[key])
+}
